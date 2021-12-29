d6a4f7c78e417cdcaa05df190302d28f3ae1d7f3
Alfredo Valle <alfredo.valle@upm.es>
2021-12-29 14:42:29 +0100

    test graph

---
+++ test/test_graph.py
@@ -0,0 +1,17 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def get_transitions(graph, start, end):
+    edge_labels = nx.get_edge_attributes(graph, 'transition')
+    path = nx.shortest_path(graph)
+    path_edges = [edge_labels.get(x, edge_labels.get((x[1], x[0]))) for x in
+                  zip(path[start][end], path[start][end][1:])]
+    return path_edges
+
+
+if __name__ == '__main__':
+    g = nx.read_graphml('../src/utils/maxon.graphml')
+    print(type(g))
+    print('graph created')
+    print(get_transitions(g, 'Fault', 'Switched on'))
