47ba15eca110d75ab452c9f62f12b262ad4fb4bb
Alfredo Valle <alfredo.valle@upm.es>
2023-02-14 09:43:01 +0100

    emergency stop

--- INSIA_control/Decision/decision_low.py
+++ INSIA_control/Decision/decision_low.py
@@ -5,6 +5,7 @@ from traceback import format_exc
 
 import rclpy
 import yaml
+from std_msgs.msg import Bool
 from insia_msg.msg import StringStamped, PetConduccion, MasterSwitch, ModoMision, Override
 from rcl_interfaces.msg import SetParametersResult
 from rclpy.node import Node
@@ -49,6 +50,7 @@ class Decision(Node):
         self.master_switch = MasterSwitch(b_steering=True, b_throttle=True, b_brake=True, b_gear=True)
         self.tele_msg = None
         self.wp_msg = None
+        self.emergency_stop_msg = False
         self.override = Override()
 
         self.wp_ttl, self.wp_mode = self.get_p(self.get_parameters_by_prefix('wp'))
@@ -62,6 +64,9 @@ class Decision(Node):
 
         self.pub_decision = self.create_publisher(msg_type=PetConduccion, topic='Decision/Output',
                                                   qos_profile=HistoryPolicy.KEEP_LAST)
+
+        self.create_subscription(msg_type=Bool, topic='EnmergencyStop', callback=self.emergency_stop_callback,
+                                 qos_profile=HistoryPolicy.KEEP_LAST)
 
         self.create_subscription(msg_type=MasterSwitch, topic='MasterSwitch', callback=self.master_switch_callback,
                                  qos_profile=HistoryPolicy.KEEP_LAST)
@@ -101,6 +106,9 @@ class Decision(Node):
         else:
             mode = 'wheels'
         return ttl, mode
+
+    def emergency_stop_callback(self, data):
+        self.emergency_stop_msg = data.data
 
     def override_callback(self, data):
         self.override = data
@@ -218,6 +226,8 @@ class Decision(Node):
         msg_final.b_throttle = msg_final.b_throttle and self.master_switch.b_throttle
         msg_final.b_steering = msg_final.b_steering and self.master_switch.b_steering
         msg_final.b_gear = msg_final.b_gear and self.master_switch.b_gear
+        if self.emergency_stop_msg:
+            msg_final.speed = 0.
         self.pub_decision.publish(msg_final)
 
     def publish_heartbeat(self):
