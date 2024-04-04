fcd1dcb1638700b7dfdc3791c76217a1673c5253
SIMCA-INSIA <simca.insia@upm.es>
2024-04-04 14:42:09 +0200

    control IA actualizado

--- INSIA_control/HLControl/LongitudinalControlIA.py
+++ INSIA_control/HLControl/LongitudinalControlIA.py
@@ -1,4 +1,6 @@
 import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 import rclpy
 import yaml
 from insia_msg.msg import Telemetry, StringStamped, PetConduccion, ControladorFloat
@@ -9,6 +11,7 @@ from rclpy.qos import HistoryPolicy
 from std_msgs.msg import Header
 from yaml.loader import SafeLoader
 import tensorflow as tf
+
 import numpy as np
 import joblib
 
@@ -35,9 +38,11 @@ class LongitudinalController(Node):
         self._log_level: Parameter = self.get_parameter_or('log_level', Parameter(name='log_level', value=10))
         self.logger.set_level(self._log_level.value)
         self.shutdown_flag = False
-        self.scaler = joblib.load('/home/simca/Escritorio/Javi/modelos_red_neuronal/scaler_nuevo.pkl')
-        #self.modelo_cargado = tf.keras.models.load_model('/home/simca/Escritorio/Javi/modelos_red_neuronal/modelo_entrenado_nuevo.h5')
-        self.modelo_cargado = tf.keras.models.load_model('/home/simca/Escritorio/Javi/modelos_red_neuronal/modelo_entrenado_nuevo.h5')
+        #self.scaler = joblib.load('/home/simca/Escritorio/Javi/modelos_red_neuronal2/scaler_nuevo.pkl')
+        self.scaler = joblib.load('/home/simca/Escritorio/Javi/modelos_red_neuronal2/mucho_frenado.pkl')
+        self.modelo_cargado = tf.keras.models.load_model('/home/simca/Escritorio/Javi/modelos_red_neuronal2/mucho_frenado.h5')
+        #self.modelo_cargado = tf.keras.models.load_model('/home/simca/Escritorio/Javi/modelos_red_neuronal2/modelo_entrenado_nuevo.h5')
+
 
         # Subscripción a los topics de telemetría, gps y velocidad objetivo
         self.telemetry_sub = self.create_subscription(Telemetry, 'Telemetry', self.telemetry_callback,
@@ -54,9 +59,8 @@ class LongitudinalController(Node):
         self.pub_heartbeat = self.create_publisher(msg_type=StringStamped, topic='Heartbeat',
                                                    qos_profile=HistoryPolicy.KEEP_LAST)
 
-
         # Variables para almacenar los valores de telemetría y velocidad objetivo
-        self.speed = 0
+        self.speed = None
         self.speed_history = []
         self.steering_history = []
         self.altitude_history = []
@@ -64,7 +68,10 @@ class LongitudinalController(Node):
         self.resta_velocidad_5 = 0
         self.giro_volante_10 = 0
 
-        self.telemetry: Telemetry = None
+        self.full_brake = False
+        self.status_brake = False
+        self.obj_speed = 0
+
         self.target: PetConduccion = None
         self.timer_heartbeat = self.create_timer(1, self.publish_heartbeat)
 
@@ -86,50 +93,93 @@ class LongitudinalController(Node):
             self.speed_history = self.speed_history[-5:]
         self.resta_velocidad_5 = self.speed_history[-1] - self.speed_history[0]
 
-        self.steering = msg.steering_deg
-        self.steering_history.append(self.steering)
+        self.steering_history.append(msg.steering_deg)
         if len(self.steering_history) > 10:
             self.steering_history = self.steering_history[-10:]
         self.giro_volante_10 = self.steering_history[-1] - self.steering_history[0]
 
 
     def gps_callback(self, msg:NavSatFix):
-        self.altitude = msg.altitude
-        self.altitude_history.append(self.steering)
+        self.altitude_history.append(msg.altitude)
         if len(self.altitude_history) > 10:
             self.altitude_history = self.altitude_history[-10:]
         self.resta_altitud_10 = self.altitude_history[-1] - self.altitude_history[0]
 
+        if self.target is not None and self.speed is not None and not self.shutdown_flag:
 
-        if self.obj_speed - self.speed > 10:
-         self.obj_speed = self.speed + 6
-        if self.obj_speed - self.speed < - 10:
-         self.obj_speed = self.speed - 6
+            if self.obj_speed - self.speed > 5:
+                self.obj_speed = self.speed + 5
+            if self.obj_speed - self.speed < -20:
+                self.obj_speed = self.speed - 20
 
-        predicciones_clasificacion, predicciones_magnitud = self.neural_network_predict(self.speed, self.obj_speed,
-                                                                                        self.resta_velocidad_5,
-                                                                                        self.giro_volante_10,
-                                                                                        self.resta_altitud_10)
-        clase_predicha = np.argmax(predicciones_clasificacion)
-        magnitud = predicciones_clasificacion[0][clase_predicha]
+            predicciones_clasificacion, predicciones_magnitud = self.neural_network_predict(self.speed, self.obj_speed,
+                                                                                            self.resta_velocidad_5,
+                                                                                               self.giro_volante_10,
+                                                                                            self.resta_altitud_10)
+            clase_predicha = np.argmax(predicciones_clasificacion)
+            magnitud = predicciones_magnitud
 
 
-        if magnitud > 60:
-            magnitud = 50
 
-        if clase_predicha == 0:
-            self.brake_pub.publish(ControladorFloat(
-                header=Header(stamp=self.get_clock().now().to_msg()),
-                enable=self.target.b_throttle,
-                target=float(magnitud/100.0)
-            ))
+            if magnitud > 77.0:
+                magnitud = 77.0
+            if magnitud < 0.0:
+                magnitud = 0.0
 
-        if clase_predicha == 1:
-            self.throttle_pub.publish(ControladorFloat(
-                header=Header(stamp=self.get_clock().now().to_msg()),
-                enable=self.target.b_brake,
-                target=float(magnitud/77.0)
-            ))
+            if self.obj_speed != 0. and self.full_brake:
+                self.full_brake = False
+
+            if self.obj_speed == 0. and self.speed < 6. and not self.full_brake:
+                self.full_brake = True
+                self.status_brake = magnitud
+
+            if not self.full_brake:
+                if clase_predicha == 0:
+                    magnitud = magnitud * 2
+                    self.brake_pub.publish(ControladorFloat(
+                        header=Header(stamp=self.get_clock().now().to_msg()),
+                        enable=self.target.b_brake,
+                        target=float(magnitud/77.0)
+                    ))
+                    self.throttle_pub.publish(ControladorFloat(
+                        header=Header(stamp=self.get_clock().now().to_msg()),
+                        enable=self.target.b_throttle,
+                        target=0.
+                    ))
+                elif clase_predicha == 1:
+                    self.throttle_pub.publish(ControladorFloat(
+                        header=Header(stamp=self.get_clock().now().to_msg()),
+                        enable=self.target.b_throttle,
+                        target=float(magnitud/100.0)
+                    ))
+                    self.brake_pub.publish(ControladorFloat(
+                        header=Header(stamp=self.get_clock().now().to_msg()),
+                        enable=self.target.b_brake,
+                        target=0.
+                    ))
+                elif clase_predicha == 2:
+                    self.brake_pub.publish(ControladorFloat(
+                        header=Header(stamp=self.get_clock().now().to_msg()),
+                        enable=self.target.b_brake,
+                        target=0.
+                    ))
+                    self.throttle_pub.publish(ControladorFloat(
+                        header=Header(stamp=self.get_clock().now().to_msg()),
+                        enable=self.target.b_throttle,
+                        target=0.
+                    ))
+            else:
+                self.status_brake += 0.05
+                if self.status_brake > 0.75:
+                    self.status_brake = 0.75
+                self.throttle_pub.publish(ControladorFloat(
+                    header=Header(stamp=self.get_clock().now().to_msg()),
+                    enable=self.target.b_throttle,
+                    target=0.))
+                self.brake_pub.publish(ControladorFloat(
+                    header=Header(stamp=self.get_clock().now().to_msg()),
+                    enable=self.target.b_brake,
+                    target=self.status_brake))
 
     def target_callback(self, msg: PetConduccion):
         # Almacenar el valor de velocidad objetivo
@@ -139,6 +189,16 @@ class LongitudinalController(Node):
     def shutdown(self):
         try:
             self.shutdown_flag = True
+            self.brake_pub.publish(ControladorFloat(
+                header=Header(stamp=self.get_clock().now().to_msg()),
+                enable=False,
+                target=0.
+            ))
+            self.throttle_pub.publish(ControladorFloat(
+                header=Header(stamp=self.get_clock().now().to_msg()),
+                enable=False,
+                target=0.
+            ))
         except Exception as e:
             self.logger.error(f'Exception in shutdown: {e}')
 
